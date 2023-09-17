b4ed873fc01c576942d2842641c6f23c68ab6a74
skypyk <skypyk@outlook.com>
2023-09-17 18:04:06 +0800

    first

---
+++ resources/js/Pages/Home/Index.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import {Head} from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="珠宝展柜陈列设计,柜台道具包装" />
+
+    <div class="min-h-full p-4">
+        <div class="">
+            <h1 class="text-black text-3xl">深圳天琚科技有限公司</h1>
+            <p class="py-4">业务范围：珠宝展柜陈列设计,柜台道具包装</p>
+        </div>
+        <div class="py-6">
+            联系人： 徐惠群， 13246649886
+        </div>
+
+        <div class="fixed mb-0 ml-0">
+            网站备案号：<a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2023100755号-1</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
